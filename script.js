40582de34df4b884dc2674f5455ab196e96a09a4
Chaimaa Lougsari <lougsari@outlook.com>
2021-12-27 21:19:16 +0100

    single page

--- script.js
+++ script.js
@@ -2,6 +2,27 @@ const mainMenu = document.querySelector('.mainMenu');
 const closeMenu = document.querySelector('.closeMenu');
 const openMenu = document.querySelector('.openMenu');
 
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+  
+
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
 
 openMenu.addEventListener('click',show);
 closeMenu.addEventListener('click',close);
@@ -15,11 +36,6 @@ function close(){
 }
 
 
-// const card= document.querySelector('.cardInner')
-// console.log('debug')
-// card.addEventListener('click', function(){
-   
-//     card.classList.toggle('isFlipped');
-  
-// })
+
+
 
